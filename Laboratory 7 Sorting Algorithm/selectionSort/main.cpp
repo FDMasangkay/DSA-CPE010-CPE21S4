c80431f326b3d644375eeaa9660b6be821ed5f61
Frederick Masangkay <qfdmasangkay@tip.edu.ph>
2024-10-18 03:30:13 +0800

    Update main.cpp

--- Laboratory 7 Sorting Algorithm/selectionSort/main.cpp
+++ Laboratory 7 Sorting Algorithm/selectionSort/main.cpp
@@ -1,40 +1,33 @@
 #include <iostream>
 #include <cstdlib>  // For rand() and srand()
 #include <ctime>    // For seeding rand()
-#include "selectionSort.h"  // Correct header inclusion
-
-using namespace std;
+#include "selectionSort.h"  // Include the header for selection sort
 #define ARRAY_SIZE 100
 
-// Function to generate a random array of integers
-void randArray(int arr[], int size) {
-    srand(time(0));  // Seed random number generator with current time
+void createRandomArray(int arr[], int size) {
+    srand(static_cast<unsigned int>(time(0)));  // Seed random number generator with current time
     for (int i = 0; i < size; i++) {
         arr[i] = rand() % 1000;  // Generate random number between 0 and 999
     }
 }
 
-// Function to print an array of integers
 void printArray(int arr[], int size) {
     for (int i = 0; i < size; i++) {
-        cout << arr[i] << " ";
+        std::cout << arr[i] << " ";
     }
-    cout << endl;
+    std::cout << std::endl;
 }
 
 int main() {
     int arr[ARRAY_SIZE];
-    
-    // Generate a random array of size ARRAY_SIZE
-    randArray(arr, ARRAY_SIZE);
+    createRandomArray(arr, ARRAY_SIZE);
 
-    cout << "Unsorted array: \n";
+    std::cout << "Unsorted Array:" << std::endl;
     printArray(arr, ARRAY_SIZE);
 
-    // Sort the array using selection sort
     selectionSort(arr, ARRAY_SIZE);
 
-    cout << "Sorted array: \n";
+    std::cout << "Sorted Array:" << std::endl;
     printArray(arr, ARRAY_SIZE);
 
     return 0;

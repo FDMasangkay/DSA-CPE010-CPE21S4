7659f69b7d95ea85e934725018f773d9113fee19
Frederick Masangkay <qfdmasangkay@tip.edu.ph>
2024-10-21 14:24:08 +0800

    Update main.cpp

--- Laboratory 8 SORTING ALGORITHMS: SHELL, MERGE, AND QUICK SORT/Merge Sort/main.cpp
+++ Laboratory 8 SORTING ALGORITHMS: SHELL, MERGE, AND QUICK SORT/Merge Sort/main.cpp
@@ -1,7 +1,17 @@
 #include <iostream>
+#include <cstdlib>  
+#include <ctime>   
 #include "mergeSort.h"
 
 using namespace std;
+#define ARRAY_SIZE 100
+
+void randArray(int arr[], int size) {
+    srand(time(0));  
+    for (int i = 0; i < size; i++) {
+        arr[i] = rand() % 100;  
+    }
+}
 
 void printArray(int arr[], int size) {
     for (int i = 0; i < size; i++) {
@@ -11,16 +21,17 @@ void printArray(int arr[], int size) {
 }
 
 int main() {
-    int arr[] = {38, 27, 43, 3, 9, 82, 10};
-    int size = sizeof(arr) / sizeof(arr[0]);
+    int arr[ARRAY_SIZE];
+
+    randArray(arr, ARRAY_SIZE);
 
     cout << "Unsorted array: \n";
-    printArray(arr, size);
+    printArray(arr, ARRAY_SIZE);
 
-    merge_sort(arr, 0, size - 1);
+    merge_sort(arr, 0, ARRAY_SIZE - 1);
 
     cout << "Sorted array: \n";
-    printArray(arr, size);
+    printArray(arr, ARRAY_SIZE);
 
     return 0;
 }

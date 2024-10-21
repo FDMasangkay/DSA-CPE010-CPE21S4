1c2bb9ddef6a4e30f38bfe812eddc62031ec5f11
Frederick Masangkay <qfdmasangkay@tip.edu.ph>
2024-10-21 14:42:42 +0800

    Create main.cpp

---
+++ Laboratory 8 SORTING ALGORITHMS: SHELL, MERGE, AND QUICK SORT/Quick Sort/main.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <cstdlib>  
+#include <ctime>   
+#include "quickSort.h"
+
+using namespace std;
+#define ARRAY_SIZE 100
+
+void randArray(int arr[], int size) {
+    srand(time(0));  
+    for (int i = 0; i < size; i++) {
+        arr[i] = rand() % 100;  
+    }
+}
+
+void printArray(int arr[], int size) {
+    for (int i = 0; i < size; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+}
+
+int main() {
+    int arr[ARRAY_SIZE];
+
+    randArray(arr, ARRAY_SIZE);
+
+    cout << "Unsorted array: \n";
+    printArray(arr, ARRAY_SIZE);
+
+    quicksort(arr, 0, ARRAY_SIZE - 1); 
+
+    cout << "Sorted array: \n";
+    printArray(arr, ARRAY_SIZE);
+
+    return 0;
+}
